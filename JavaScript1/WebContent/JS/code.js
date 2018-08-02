218dfb97fe1a279cdee2059d6d76e3997671799f
madhu12 <madhu12@192.168.1.31>
2018-08-02 19:25:39 +0530

    JAVASCRIPT

---
+++ JavaScript1/WebContent/JS/code.js
@@ -0,0 +1,27 @@
+function display(){
+	var hd = document.getElementById('hellodiv');
+	hd.innerHTML = 'CLICKY Y Y ';
+}
+
+function greetings(){
+
+var myFunction=function(var1,var2){
+		document.write(var1+var2);
+}
+var sum=myFunction(14,15);
+
+
+}
+
+function test1(){
+	var var1=1;
+	alert(var1);
+}
+
+test1();
+
+function test2(){
+	alert(var1);
+}
+
+test2();
